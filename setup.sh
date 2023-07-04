18e092feb750c3d514550a40bfd471e2acb96a83
jng <junhanngd1115@gmail.com>
2023-07-04 16:41:04 +0800

    init

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 ver=$(uname -r)
-cp -r ft_syscall/ /usr/src/kernel-$ver}/linux-$ver/
+cp -r ft_syscall/ /usr/src/kernel-$ver/linux-$ver/
 echo "666	common	get_pid_info	sys_get_pid_info" >> /usr/src/kernel-$ver/linux-$ver/arch/x86/entry/syscalls/syscall_64.tbl
 echo "asmlinkage long sys_get_pid_info();" >> /usr/src/kernel-$ver/linux-$ver/include/linux/syscalls.h
 # edit makefile here??
