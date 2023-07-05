7819033a9a75f7e3ae082e6d79aa16eeb6660423
jng <junhanngd1115@gmail.com>
2023-07-05 09:47:14 +0800

    makefile changes

--- sys_get_pid_info/sys_get_pid_info.c
+++ sys_get_pid_info/sys_get_pid_info.c
@@ -1,13 +1,13 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
 
-asmlinkage long int __x64_sys_get_pid_info(void)
-{
-    return sys_get_pid_info();
-}
-
 asmlinkage long sys_get_pid_info(void)
 {
     printk("hello world!!!!\n");
     return 0;
+}
+
+asmlinkage long __x64_sys_get_pid_info(void)
+{
+    return sys_get_pid_info();
 }
