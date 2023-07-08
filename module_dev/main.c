fc8ca34dc95c42ac47912e87c5d305f334f40161
jng <junhanngd1115@gmail.com>
2023-07-09 06:50:38 +0800

    feat: kernel data generation; todo children

--- module_dev/main.c
+++ module_dev/main.c
@@ -1,11 +1,11 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
-#include <linux/sched.h>
 #include <linux/ktime.h>
 #include <linux/uaccess.h>
 #include <linux/list.h>
 #include <linux/fs_struct.h>
 #include <linux/dcache.h>
+#include <linux/sched.h>
 
 struct pid_info
 {
@@ -22,7 +22,7 @@ struct pid_info
 static struct pid_info create_pid_info(int pid)
 {
 	struct pid_info res;
-	struct task_struct *task = find_task_by_vpid(pid);
+	struct task_struct *task = current;
 
 	res.pid = task->pid;
 	res.state = task->state;
