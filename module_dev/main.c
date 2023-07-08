b3de9b17e91e9e378edef743c5968ea53c261be5
jng <junhanngd1115@gmail.com>
2023-07-09 07:55:13 +0800

    feat: kernel data generation; todo children

--- module_dev/main.c
+++ module_dev/main.c
@@ -50,7 +50,7 @@ static struct pid_info create_pid_info(int pid)
 
 	struct list_head *curr_child = og_child->next;
 	child_task = list_entry(curr_child, struct task_struct, children);
-	printk("next child addr %p, pid %d\n", child_task, child_task->pid);
+	printk("next child addr %p, pid %d\n", child_task, og_child->pid);
 	// while (curr_child != og_child)
 	// {
 	// 	// add subsequent children...
