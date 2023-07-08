9d8963da1d390cfe5b3b6eeba57e49171a04a9af
jng <junhanngd1115@gmail.com>
2023-07-09 06:42:59 +0800

    feat: kernel data generation; todo children

--- module_dev/main.c
+++ module_dev/main.c
@@ -9,13 +9,63 @@
 MODULE_AUTHOR("jng");
 MODULE_LICENSE("GPL");
 
+struct pid_info
+{
+	long     pid;
+	long     state;
+	void*   process_stack;
+	long    age;
+	long*   children;
+	long	parent_pid;
+	char*	root;
+	char*	pwd;
+};
+
+static struct pid_info create_pid_info(int pid)
+{
+	struct pid_info res;
+	struct task_struct *task = find_task_by_vpid(pid);
+
+	res.pid = task->pid;
+	res.state = task->state;
+	res.process_stack = task->stack;
+	// age...
+	s64  uptime;
+    uptime = ktime_divns((ktime_get_boottime() * 1000), NSEC_PER_SEC);
+	res.age = uptime - (task->start_time - _SC_CLK_TCK);
+
+	// // children...
+	// struct list_head og_child = task->children;
+
+	// // add first child
+	// // struct list_head head = list_entry(og_child, struct task_struct, children);
+	// struct task_struct *child_task = list_entry(&og_child, struct task_struct, children);
+	// if (child_task == 0)
+	// 	printk("first child %d\n", child_task->pid);
+
+	// struct list_head curr_child = *(og_child.next);
+	// while (&(curr_child) != &(og_child))
+	// {
+	// 	// add subsequent children...
+	// 	child_task = list_entry(&curr_child, struct task_struct, children);
+	// 	printk("next child %d\n", child_task->pid);
+	// 	curr_child = *(curr_child.next);
+	// }
+	
+
+	res.parent_pid = task->real_parent->pid;
+	res.root = task->fs->root.dentry->d_name.name;
+	res.pwd = task->fs->pwd.dentry->d_name.name;
+
+	return res;
+}
 
 /**
  * Module Init. Registers a USB device and creates a misc device in /dev/ft_module_keyboard
 */
 int init_module(void)
 {
-	printk("hello worldddd\n");
+	struct pid_info res = create_pid_info(current->pid);
 	return 0;
 }
 
