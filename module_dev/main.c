cccd5074c44bd3dd91448767590f610b346a8e77
jng <junhanngd1115@gmail.com>
2023-07-09 07:53:19 +0800

    feat: kernel data generation; todo children

--- module_dev/main.c
+++ module_dev/main.c
@@ -67,8 +67,8 @@ static struct pid_info create_pid_info(int pid)
 */
 int init_module(void)
 {
-	printk("currpid %d\n\n", 1);
-	struct pid_info res = create_pid_info(1);
+	printk("currpid %d\n\n", current->pid);
+	struct pid_info res = create_pid_info(current->pid);
 	return 0;
 }
 
