c75a9662ee2981307c848c19ad037e136eb71292
jng <junhanngd1115@gmail.com>
2023-07-08 20:57:38 +0800

    feat: kernel data generation; todo children

--- sys_get_pid_info/sys_get_pid_info.c
+++ sys_get_pid_info/sys_get_pid_info.c
@@ -36,7 +36,7 @@ static struct pid_info create_pid_info(int pid)
 
 	// add first child
 	// struct list_head head = list_entry(og_child, struct task_struct, children);
-	struct task_struct child_task = list_entry(og_child, struct task_struct, children);
+	struct task_struct child_task = list_entry(&og_child, struct task_struct, children);
 	if (child_task)
 		printk("first child %d\n", child_task->pid);
 
@@ -44,7 +44,7 @@ static struct pid_info create_pid_info(int pid)
 	while (&(curr_child) != &(og_child))
 	{
 		// add subsequent children...
-		child_task = list_entry(curr_child, struct task_struct, children);
+		child_task = list_entry(&curr_child, struct task_struct, children);
 		printk("next child %d\n", child_task.pid);
 		curr_child = *(curr_child.next);
 	}
