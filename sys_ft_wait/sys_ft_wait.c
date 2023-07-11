7a8e5e5cb493a38b5360a7a4b1ac53c9b4326d43
jng <junhanngd1115@gmail.com>
2023-07-11 21:51:49 +0800

    feat: wait boiler

--- sys_ft_wait/sys_ft_wait.c
+++ sys_ft_wait/sys_ft_wait.c
@@ -1,7 +1,7 @@
 #include <linux/linkage.h>
 #include <linux/syscalls.h>
 
-SYSCALL_DEFINE2(ft_wait, int __user *, status)
+SYSCALL_DEFINE(ft_wait, int __user *, status)
 {
 	unsigned long max = 	2147483647;
 	while (max-- > 1)
---
+++ tests/wait_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int gay;
+
+	printf("syscall begin\n");
+	syscall(353, &gay);
+	printf("syscall over\n");
+	return 0;
+}
