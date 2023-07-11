09e3db92bae21b29e6d7085aa5ef92688565cc9f
jng <junhanngd1115@gmail.com>
2023-07-11 16:33:10 +0800

    feat: add process not found and ft_kill boiler

--- sys_ft_kill/sys_ft_kill.c
+++ sys_ft_kill/sys_ft_kill.c
@@ -3,9 +3,23 @@
 #include <linux/sched.h>
 #include <linux/syscalls.h>
 
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/ktime.h>
+#include <linux/uaccess.h>
+#include <linux/list.h>
+#include <linux/fs_struct.h>
+#include <linux/dcache.h>
+#include <linux/sched.h>
+#include <linux/pid.h>
+#include <linux/timekeeping.h>
+#include <linux/slab.h>
+#include <linux/syscalls.h>
+#include <linux/string.h>
+
 SYSCALL_DEFINE2(ft_kill, long, pid, int, sig)
 {
-	printk("ft_kill\n");
+	printk("ft_kill %ld, %d\n", pid, sig);
 	// struct siginfo info;
 	// struct task_struct *task = find_task_by_vpid(pid);
 
