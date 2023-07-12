0f5ddd09d395d6b8d93586e5b21d536c2a2916ed
jng <junhanngd1115@gmail.com>
2023-07-12 10:01:42 +0800

    feat: wait boiler

--- tests/wait_test.c
+++ tests/wait_test.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
 	int gay;
 
-	printf("syscall begin pid %d\n", getpid());
-	syscall(353, &gay);
-	printf("syscall over\n");
+	int pid = fork();
+
+	if (!pid)
+	{
+		while (1){}
+	}
+	else
+	{
+		printf("syscall begin pid %d\n", getpid());
+		syscall(353, &gay);
+		printf("syscall over\n");
+	}
 	return 0;
 }
