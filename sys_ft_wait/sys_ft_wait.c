9344333589ba15fd9bba76e4e896236a30f9a9a3
jng <junhanngd1115@gmail.com>
2023-07-12 08:15:08 +0800

    feat: wait boiler

--- sys_ft_wait/sys_ft_wait.c
+++ sys_ft_wait/sys_ft_wait.c
@@ -3,11 +3,18 @@
 
 SYSCALL_DEFINE1(ft_wait, int __user *, status)
 {
+	// testing stuff
+	current->state = TASK_INTERRUPTIBLE;
+
+
+	// set current state to sleep
+
+	// start loop 
+		// check if current state changed
+
+		// iterate children to check if any of them return (change state to exit zombie)
+
+		// if one of them does, change child state to exit dead and return status code
 	printk("kernel pid %d\n", current->pid);
-	unsigned long max = 	2147483647;
-	while (1)
-	{
-	}
-	
 	return 0;
 }
