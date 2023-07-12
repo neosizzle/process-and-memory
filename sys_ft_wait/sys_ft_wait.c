b007912ecc8d0d6d04bf6947e3ede050b82080ba
jng <junhanngd1115@gmail.com>
2023-07-12 09:31:44 +0800

    feat: wait boiler

--- sys_ft_wait/sys_ft_wait.c
+++ sys_ft_wait/sys_ft_wait.c
@@ -16,7 +16,7 @@ SYSCALL_DEFINE1(ft_wait, int __user *, status)
 	struct wait_queue_entry wait;
 	init_waitqueue_entry(&wait, current);
 	current->state = TASK_INTERRUPTIBLE;
-	add_wait_queue(my_wait_queue,&wait); /* wq points to the wait queue head */
+	add_wait_queue(&my_wait_queue,&wait); /* wq points to the wait queue head */
 	schedule();
 	remove_wait_queue(my_wait_queue, &wait);
 
