ec996d7495adf9f8d5c1119553a8ffd47bbf90d0
jng <junhanngd1115@gmail.com>
2023-07-12 08:33:38 +0800

    feat: wait boiler

--- sys_ft_wait/sys_ft_wait.c
+++ sys_ft_wait/sys_ft_wait.c
@@ -1,11 +1,18 @@
 #include <linux/linkage.h>
 #include <linux/syscalls.h>
 #include <linux/sched.h>
+#include <linux/wait.h>
 
 SYSCALL_DEFINE1(ft_wait, int __user *, status)
 {
 	// testing stuff
 	current->state = TASK_INTERRUPTIBLE;
+
+	DECLARE_WAIT_QUEUE_HEAD(my_wait_queue);
+	wait_event(my_wait_queue, 0);
+	printf("wait_event return\n");
+	// schedule();
+
 
 
 	// set current state to sleep
