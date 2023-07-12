beeea8f1460f777f4a6f5c1ca80900623f7c1462
jng <junhanngd1115@gmail.com>
2023-07-12 11:11:12 +0800

    feat: wait boiler

--- sys_ft_wait/sys_ft_wait.c
+++ sys_ft_wait/sys_ft_wait.c
@@ -14,11 +14,11 @@ SYSCALL_DEFINE1(ft_wait, int __user *, status)
 	// testing stuff
 	// current->state = TASK_INTERRUPTIBLE;
 
-	DECLARE_WAIT_QUEUE_HEAD(my_wait_queue);
+	// DECLARE_WAIT_QUEUE_HEAD(my_wait_queue);
 
 
-	wait_event_interruptible(my_wait_queue, condition_check());
-	printk("wait_event return\n");
+	// wait_event_interruptible(my_wait_queue, condition_check());
+	// printk("wait_event return\n");
 	
 	// struct wait_queue_entry wait;
 	// init_waitqueue_entry(&wait, current);
@@ -31,13 +31,29 @@ SYSCALL_DEFINE1(ft_wait, int __user *, status)
 
 
 	// set current state to sleep
+	current->state = TASK_INTERRUPTIBLE;
 
-	// start loop 
+	// start loop
+	while (1)
+	{
 		// check if current state changed
 
 		// iterate children to check if any of them return (change state to exit zombie)
+		list_for_each_entry(child_task, &task->children, sibling) {
+			printk("child status %d, exit_state %d, exit_code %d, exit_signal %d\n",
+			child_task->state,
+			child_task->exit_state,
+			child_task->exit_code,
+			child_task->exit_signal
+			);
 
-		// if one of them does, change child state to exit dead and return status code
+			// if one of them dies, change child state to exit dead and return status code
+			if (child_task->state > 0)
+				break ;
+		}
+
+	}
+	
 	printk("queue return, kernel pid %d\n", current->pid);
 	return 0;
 }
