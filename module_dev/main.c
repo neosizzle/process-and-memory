617165052c28af21cb0e48adbd84bbbbd107a0f8
jng <junhanngd1115@gmail.com>
2023-07-16 16:34:14 +0800

    feat: module dev

--- module_dev/main.c
+++ module_dev/main.c
@@ -12,6 +12,30 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 
+#include <linux/slab.h>
+
+// static inline struct task_struct *ft_alloc_task_struct_node(int node)
+// {
+// 	return kmem_cache_alloc_node(task_struct_cachep, GFP_KERNEL, node);
+// }
+
+static struct task_struct * ft_dup_task_struct(struct task_struct *orig, int node)
+{
+	struct task_struct *tsk;
+
+	// non uniform memory address node
+	if (node == NUMA_NO_NODE)
+		node = tsk_fork_get_node(orig);
+
+	// allocate a fresh task_struct with the numa node type
+	tsk = alloc_task_struct_node(node);
+	if (!tsk)
+		return NULL;
+
+	*tsk = *orig;
+	return tsk;
+}
+
 static struct task_struct *ft_copy_process(
 					unsigned long clone_flags,
 					unsigned long stack_start,
@@ -22,6 +46,22 @@ static struct task_struct *ft_copy_process(
 					unsigned long tls,
 					int node)
 {
+	struct task_struct *tsk;
+
+	// check for flag compatibility
+	if ((clone_flags & (CLONE_NEWNS|CLONE_FS)) == (CLONE_NEWNS|CLONE_FS))
+		return ERR_PTR(-EINVAL);
+	
+	if ((clone_flags & CLONE_THREAD) && !(clone_flags & CLONE_SIGHAND))
+		return ERR_PTR(-EINVAL);
+	
+	if ((clone_flags & CLONE_SIGHAND) && !(clone_flags & CLONE_VM))
+		return ERR_PTR(-EINVAL);
+
+	// security checks (ommiting :P)
+
+	// dup_task_struct
+	tsk = dup_task_struct(current, node);
 	return 0;
 }
 
@@ -31,7 +71,16 @@ static struct task_struct *ft_copy_process(
 */
 int init_module(void)
 {
-	
+	ft_copy_process(
+		SIGCHLD,
+		0,
+		0,
+		NULL,
+		NULL,
+		0,
+		0,
+		NUMA_NO_NODE
+		);
 	return 0;
 }
 
